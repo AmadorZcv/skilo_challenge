3be1075b526040b4cf01da1fcf7cecab8b6dfcc0
AmadorZcv <lucasamadorzcv@gmail.com>
2019-11-03 14:49:39 -0300

    Retornando uuid no accounts
    Signed-off-by: AmadorZcv <lucasamadorzcv@gmail.com>

--- lib/skilo_challenge_web/resolvers/account.ex
+++ lib/skilo_challenge_web/resolvers/account.ex
@@ -1,5 +1,15 @@
 defmodule SkiloChallengeWeb.Resolvers.Account do
+  alias SkiloChallenge.Accounts.Account
+
+  defp to_graphql(%Account{id: id, current_balance: current_balance}) do
+    %{uuid: id, current_balance: current_balance}
+  end
+
   def list_accounts(_parent, _args, _resolution) do
-    {:ok, SkiloChallenge.Accounts.list_accounts()}
+    accounts =
+      SkiloChallenge.Accounts.list_accounts()
+      |> Enum.map(&to_graphql/1)
+
+    {:ok, accounts}
   end
 end
